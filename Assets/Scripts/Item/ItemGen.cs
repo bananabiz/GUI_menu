c70c2560e5421677a5f4767965b65bcb1c004718
EricW <lee21acc973@163.com>
2017-11-04 12:28:48 +1100

    pre inventory item setup

--- Assets/Scripts/Item/ItemGen.cs
+++ Assets/Scripts/Item/ItemGen.cs
@@ -40,6 +40,15 @@ public class ItemGen : MonoBehaviour
                 type = ItemType.Food;
                 break;
 
+            case 2:
+                name = "Meat";
+                value = 5;
+                description = "Retore half quarter of HP";
+                icon = "Meat";
+                mesh = "Meat";
+                type = ItemType.Food;
+                break;
+
             // repeat to case 99 (the last item)
             #endregion
 
@@ -54,29 +63,233 @@ public class ItemGen : MonoBehaviour
                 break;
 
             case 101:
-
+                name = "Bow";
+                value = 7;
+                description = "Bamboo Bow";
+                icon = "Bow";
+                mesh = "Bow";
+                type = ItemType.Weapon;
                 break;
+
+            case 102:
+                name = "Sword";
+                value = 10;
+                description = "Iron Sword";
+                icon = "Sword";
+                mesh = "Sword";
+                type = ItemType.Weapon;
+                break;
+
             #endregion
 
             #region Apparel 200-299
+            case 200:
+                name = "Armor";
+                value = 10;
+                description = "Copper Armor";
+                icon = "Armor";
+                mesh = "Armor";
+                type = ItemType.Apparel;
+                break;
+
+            case 201:
+                name = "Cloak";
+                value = 7;
+                description = "Leather Cloak";
+                icon = "Cloak";
+                mesh = "Cloak";
+                type = ItemType.Apparel;
+                break;
+
+            case 202:
+                name = "Pants";
+                value = 6;
+                description = "Leather Pants";
+                icon = "Pants";
+                mesh = "Pants";
+                type = ItemType.Apparel;
+                break;
+
+            case 203:
+                name = "Helmet";
+                value = 6;
+                description = "Iron Helmet";
+                icon = "Helmet";
+                mesh = "Helmet";
+                type = ItemType.Apparel;
+                break;
+
+            case 204:
+                name = "Bag";
+                value = 5;
+                description = "Leather Bag";
+                icon = "Bag";
+                mesh = "Bag";
+                type = ItemType.Apparel;
+                break;
+
+            case 205:
+                name = "Boots";
+                value = 5;
+                description = "Leather Boots";
+                icon = "Boots";
+                mesh = "Boots";
+                type = ItemType.Apparel;
+                break;
+
+            case 206:
+                name = "Gloves";
+                value = 5;
+                description = "Leather Gloves";
+                icon = "Gloves";
+                mesh = "Gloves";
+                type = ItemType.Apparel;
+                break;
+
+            case 207:
+                name = "Bracers";
+                value = 5;
+                description = "Leather Bracers";
+                icon = "Bracers";
+                mesh = "Bracers";
+                type = ItemType.Apparel;
+                break;
+
+            case 208:
+                name = "Belt";
+                value = 5;
+                description = "Leather Belt";
+                icon = "Belt";
+                mesh = "Belt";
+                type = ItemType.Apparel;
+                break;
+
+            case 209:
+                name = "Belt";
+                value = 5;
+                description = "Leather Belt";
+                icon = "Belt";
+                mesh = "Belt";
+                type = ItemType.Apparel;
+                break;
+
+            case 210:
+                name = "Shoulders";
+                value = 5;
+                description = "Leather Shoulders";
+                icon = "Shoulders";
+                mesh = "Shoulders";
+                type = ItemType.Apparel;
+                break;
+
+            case 211:
+                name = "Shield";
+                value = 5;
+                description = "Wooden Shield";
+                icon = "Shield";
+                mesh = "Shield";
+                type = ItemType.Apparel;
+                break;
+
             #endregion
 
             #region Crafting 300-399
+            case 300:
+                name = "Necklace";
+                value = 10;
+                description = "Blue Gem Necklace";
+                icon = "Necklace";
+                mesh = "Necklace";
+                type = ItemType.Crafting;
+                break;
+
+            case 301:
+                name = "Ring";
+                value = 10;
+                description = "Golden Ring";
+                icon = "Ring";
+                mesh = "Ring";
+                type = ItemType.Crafting;
+                break;
+
             #endregion
 
             #region Quest 400-499
             #endregion
 
             #region Money 500-599
+            case 500:
+                name = "Coins";
+                value = 1;
+                description = "Silver Coins";
+                icon = "Coins";
+                mesh = "Coins";
+                type = ItemType.Money;
+                break;
+
+            case 501:
+                name = "Gem";
+                value = 100;
+                description = "Blue Gem";
+                icon = "Gem";
+                mesh = "Gem";
+                type = ItemType.Money;
+                break;
+
+            case 502:
+                name = "Ingots";
+                value = 20;
+                description = "Silver Ingots";
+                icon = "Ingots";
+                mesh = "Ingots";
+                type = ItemType.Money;
+                break;
+
             #endregion
 
             #region Ingredients 600-699
             #endregion
 
             #region Potions 700-799
+            case 700:
+                name = "HP";
+                value = 30;
+                description = "Restore half HP";
+                icon = "HP";
+                mesh = "HP";
+                type = ItemType.Potions;
+                break;
+
+            case 701:
+                name = "MP";
+                value = 20;
+                description = "Restore half MP";
+                icon = "MP";
+                mesh = "MP";
+                type = ItemType.Potions;
+                break;
+
             #endregion
 
             #region Scrolls 800-899
+            case 800:
+                name = "Scroll";
+                value = 5;
+                description = "Character Scroll";
+                icon = "Scroll";
+                mesh = "Scroll";
+                type = ItemType.Scrolls;
+                break;
+
+            case 801:
+                name = "Book";
+                value = 15;
+                description = "Story Book";
+                icon = "Book";
+                mesh = "Book";
+                type = ItemType.Scrolls;
+                break;
+                
             #endregion
 
             // very important, set default in case something wrong
